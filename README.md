b236e023e3fbd80416df957603e21d95cf63344d
Orçun Çolak <orcun.colak@hazelcast.com>
2024-02-28 11:46:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Read me
+
+The original idea is from  
+https://levelup.gitconnected.com/sanitize-actuator-endpoint-with-sprint-boot-3-5e2c4edae1c9
+
+# Env Actuator
+
+Go to  
+http://localhost:8080/actuator/env
---
+++ src/main/java/com/colak/springactuatorenvtutorial/SpringActuatorEnvTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.colak.springactuatorenvtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorEnvTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorEnvTutorialApplication.class, args);
+	}
+
+}
